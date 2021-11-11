e26be71efcfd9be2355f5f71709e88540f41f735
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-11-11 12:27:01 +0100

    add Header and Footer start template

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,14 +3,16 @@ import '../App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Routes>
-        <Route path="/*" element={<Header />} />
         <Route path="/*" element={<Main />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
 import { Link, useLocation } from 'react-router-dom';
+import './Header.css';
 
 function Header() {
   const location = useLocation();
   // {location.pathname === "/" ? '' : 'Главная'}
   return (
     <header className="header">
-      <Link to="/">Главная</Link>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item"><Link className="nav__link" to="/">Main</Link></li>
+          <li className="nav__item"><Link className="nav__link" to="/works">My works</Link></li>
+        </ul>
+      </nav>
     </header>
   )
 }
